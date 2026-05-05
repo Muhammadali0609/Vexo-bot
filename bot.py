9edbcc2b54b9256bc0eba6847cc23d908e955b47
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-05 23:44:44 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,29 +6,53 @@ from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filte
 from config import TOKEN
 from downloader import download_tiktok
 
+def progress_bar(percent: int):
+    filled = "█" * (percent // 10)
+    empty = "░" * (10 - percent // 10)
+    return f"{filled}{empty} {percent}%"
+    
+async def animate_loading(msg, stop_flag):
+    percent = 0
 
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    while not stop_flag["done"]:
+        percent += 10
+        if percent > 90:
+            percent = 10
+
+        await msg.edit_text(f"⬇️ Загрузка...\n{progress_bar(percent)}")
+
+        await asyncio.sleep(0.8)
+
+async def handle_message(update, context):
     text = update.message.text
 
     if "tiktok.com" not in text:
         await update.message.reply_text("Отправь TikTok ссылку 📎")
         return
 
-    status_msg = await update.message.reply_text("Скачиваю видео... ⏳")
+    msg = await update.message.reply_text("⬇️ Подготовка...")
+
+    stop_flag = {"done": False}
+
+    # запускаем анимацию
+    task = asyncio.create_task(animate_loading(msg, stop_flag))
 
     try:
-        # 🔥 ВАЖНО: уводим блокирующий код в отдельный поток
         file_path = await asyncio.to_thread(download_tiktok, text)
+
+        stop_flag["done"] = True
+        await task
+
+        await msg.edit_text("⬆️ Отправка видео...")
 
         with open(file_path, "rb") as video:
             await update.message.reply_video(video=video)
 
-        await status_msg.delete()
+        await msg.delete()
 
-        os.remove(file_path)
-
-    except Exception as e:
-        await status_msg.edit_text("Ошибка, попробуйте еще раз ❌")
+    except Exception:
+        stop_flag["done"] = True
+        await msg.edit_text("❌ Ошибка загрузки")
 
 
 app = ApplicationBuilder().token(TOKEN).build()
