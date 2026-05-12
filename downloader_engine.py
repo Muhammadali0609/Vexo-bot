1d76469c4d4de1cdd40e3697f5766eaeffeab65f
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-12 21:47:42 +0500

    Update downloader_engine.py

--- downloader_engine.py
+++ downloader_engine.py
@@ -51,6 +51,7 @@ def get_ydl_opts(platform, file_name):
     elif platform == "instagram":
         base.update({
             "format": "bestvideo*+bestaudio/best",
+            "recodevideo": "mp4",
         })
 
     # =========================
