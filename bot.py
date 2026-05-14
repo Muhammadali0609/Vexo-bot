92f0bfd953f85aba202c7bb223d58d8a06985b48
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-14 06:36:00 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import requests
 from telegram import Update, InputMediaPhoto
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (ApplicationBuilder,MessageHandler,CommandHandler,CallbackQueryHandler,ContextTypes,filters,)
-from db import add_user, get_users_count, add_event, get_cached_video, save_cached_video, update_event_status, init_db, set_user_lang, get_user_lang
+from db import add_user, get_users_count, add_event, get_cached_video, save_cached_video, update_event_status, init_db, set_user_lang, get_user_lang, is_user_banned
 from config import TOKEN, WEBHOOK_URL
 from admin import adminm, admin_callback
 from downloader_engine import download_manager, safe_remove, download_audio, get_video_metadata
