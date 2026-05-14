0fd6b9eb756df3ac23fb377764dde7a7dbaacf44
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-14 21:58:14 +0500

    Update downloader_engine.py

--- downloader_engine.py
+++ downloader_engine.py
@@ -43,7 +43,7 @@ def get_ydl_opts(platform, file_name):
                     "player_client": ["android"]
                 }
             },
-            #"cookiefile": "cookies/youtube.txt"
+            "cookiefile": "cookies/youtube.txt"
         })
 
     # =========================
