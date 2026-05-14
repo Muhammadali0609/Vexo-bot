8462acc4fd306ba5fa595d0504c811973cd84a0d
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-15 04:47:33 +0500

    Update photo_downloader.py

--- photo_downloader.py
+++ photo_downloader.py
@@ -29,35 +29,52 @@ async def download_instagram_photo(url: str):
 async def download_tiktok_photo(url: str):
     print("TIKTOK PHOTO URL:", url)
     try:
-
         response = requests.post(
-
             "https://tikwm.com/api/",
-
             data={
-
                 "url": url
-
             },
-
             timeout=20
-
         )
-
         data = response.json()
-
         images = data.get("data", {}).get("images")
-
         if not images:
-
             print("NO IMAGES")
+            return None
+        return images
+    except Exception as e:
+        print("TIKTOK PHOTO ERROR:", e)
+        return None
 
+async def download_youtube_video(url: str):
+    print("YOUTUBE URL:", url)
+    api_url = "https://api.cobalt.tools/api/json"
+    payload = {
+        "url": url
+    }
+
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                api_url,
+                json=payload,
+                headers=headers,
+                timeout=aiohttp.ClientTimeout(total=30)
+            ) as response:
+                data = await response.json()
+
+        print("COBALT RESPONSE:", data)
+
+        if data.get("status") not in ["stream", "redirect"]:
             return None
 
-        return images
+        return data.get("url")
 
     except Exception as e:
-
-        print("TIKTOK PHOTO ERROR:", e)
-
+        print("YOUTUBE ERROR:", e)
         return None
