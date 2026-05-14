ed25649f7de246dd67d86eb04fbcf29c36fb8156
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-14 21:44:15 +0500

    Update downloader_engine.py

--- downloader_engine.py
+++ downloader_engine.py
@@ -38,14 +38,7 @@ def get_ydl_opts(platform, file_name):
             "quiet": False,
             "retries": 10,
             "fragment_retries": 10,
-            "http_headers": {
-                "User-Agent": (
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                    "AppleWebKit/537.36 "
-                    "(KHTML, like Gecko) "
-                    "Chrome/124.0 Safari/537.36"
-                )
-            },
+            
             "cookiefile": "cookies/youtube.txt"
         })
 
