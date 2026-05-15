14e9c56ff5642525708f5d2a031b8621047a7ef2
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-15 05:10:12 +0500

    Update photo_downloader.py

--- photo_downloader.py
+++ photo_downloader.py
@@ -2,7 +2,6 @@ import aiohttp
 import requests
 import re
 import json
-import tempfile
 
 # =========================
 # 📸 INSTAGRAM PHOTO / CAROUSEL
@@ -47,56 +46,3 @@ async def download_tiktok_photo(url: str):
         print("TIKTOK PHOTO ERROR:", e)
         return None
 
-async def download_youtube_video(url: str):
-    print("YOUTUBE URL:", url)
-    api_url = "https://api.cobalt.tools/api/json"
-    payload = {
-        "url": url
-    }
-
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.post(
-                api_url,
-                json=payload,
-                headers=headers,
-                timeout=aiohttp.ClientTimeout(total=30)
-            ) as response:
-                data = await response.json()
-
-        print("COBALT RESPONSE:", data)
-
-        if data.get("status") not in ["stream", "redirect"]:
-            return None
-
-        return data
-
-    except Exception as e:
-        print("YOUTUBE ERROR:", e)
-        return None
-
-async def download_file(url):
-
-    temp_file = tempfile.NamedTemporaryFile(
-        delete=False,
-        suffix=".mp4"
-    )
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-
-            with open(temp_file.name, "wb") as f:
-                while True:
-                    chunk = await response.content.read(1024 * 256)
-
-                    if not chunk:
-                        break
-
-                    f.write(chunk)
-
-    return temp_file.name
